07953345274741796450f0f2a919a752d25b9feb
cgberlin <codyberlin@gmail.com>
2016-08-07 15:16:26 -0700

    fully functional

--- js/model.js
+++ js/model.js
@@ -1,19 +1,22 @@
 var spacesAlreaduUsed = [];
-
+var numberOfMoves = 0;
 function getRandomNumber() {
     return Math.floor(Math.random() * (9 - 1 + 1)) + 1;
 }
 $('.container').on('click', 'div', function(){
+  numberOfMoves++;
   if ($(this).children().length <= 0){
     var theirNumber = $(this).attr('id');
     theirNumber = parseInt(theirNumber);
     spacesAlreaduUsed.push(theirNumber);
     $(this).append('<p>X</p>');
-    checkForWin();
     getNextMove();
   }
   else {alert('tile taken')};
+});
 
+$('#reset').on('click', function(){
+  resetGame();
 });
 
 function getNextMove(){
@@ -25,11 +28,14 @@ function getNextMove(){
   spacesAlreaduUsed.push(ourNextMove);
   var divToInsert = '#'+ourNextMove;
   $(divToInsert).append('<p>O</p>');
+  if (numberOfMoves >= 3){
   checkForWin();
+}
 }
 
 function winner(whoWon){
   alert(whoWon +" " + 'WINS');
+  resetGame();
 }
 
 function checkForWin() {
@@ -48,4 +54,30 @@ function checkForWin() {
   else if ((topLeft === midMid) && (midMid === bottomRight)){
     winner(topLeft);
   }
+  else if ((midLeft === midMid) && (midMid === midRight)){
+    winner(midLeft);
+  }
+  else if ((bottomLeft === bottomMid) && (bottomMid === bottomRight)){
+    winner(bottomLeft);
+  }
+  else if ((topMid === midMid) && (midMid === bottomMid)){
+    winner(topMid);
+  }
+  else if ((topRight === midRight) && (midRight === bottomRight)){
+    winner(topRight);
+  }
+  else if ((topRight === midMid) && (midMid === bottomLeft)){
+    winner(topRight);
+  }
+  else if ((topLeft === midLeft) && (midLeft === bottomLeft)){
+    winner(topLeft);
+  }
+}
+
+function resetGame() {
+  spacesAlreaduUsed = [];
+  numberOfMoves = 0;
+  var templateForReset = "<div class = 'top-left' id = '1'></div><div class = 'top-mid' id = '2'></div><div class = 'top-right' id = '3'></div><div class = 'mid-left' id = '4'>" +
+  "</div><div class = 'mid-mid' id = '5'></div><div class = 'mid-right' id = '6'></div><div class = 'bottom-left' id = '7'></div><div class = 'bottom-mid' id = '8'></div><div class = 'bottom-right' id = '9'></div>";
+  $('.container').html(templateForReset);
 }
